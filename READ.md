27bc89238001c9d683b23d7669e2776d47acc60c
oliviabanerjee01 pop <oliviabanerjee01@gmail.com>
2026-05-04 20:29:06 +0530

    my temperature converter project

--- READ.md
+++
@@ -1,8 +0,0 @@
-# Calculator project
-My first python project.
-## What it does
-A basic calculator app that can add , substract, multiply, and divide
-### What I learnt
--Functions
--Variables
--If/Else conditions
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-num1 = int(input("enter your number: "))
-num2 = int(input("enter your number: "))
-operation = input ("choose your operayion + - * / : ")
-if operation == "+":
-    print(num1+num2)
-elif operation == "-":
-    print(num1 - num2)
-elif operation == "*":
-    print(num1 * num2)
-elif operation == "/":
-    print(num1/num2)
+temperature = int(input("enter the current temperature in your surrounding:"))
+degree = input("choose your measure Celsius, Fahrenheit or Kelvin: ")
+if degree == "Celsius":
+    
+    print("the temperature in fahrenheit is",temperature*1.8+32, "F")
+    print("the temperature in kelvin is", temperature + 273.18, "K")
+
+elif degree == "Farenheit":
+    print("the temperature in celsius is", [temperature-32]*0.55, "°C")
+    print("the temperature in kelvin is",[temperature - 32]*0.55+32, "K")
+elif degree == "Kelvin":
+    print("the temperature in celsius is", temperature-273.15, "°C")
+    print("the temperature in fahrenheit is", [temperature-273.15]*1.8+32, "F")
+
 else:
-    print("please put a valid operation amongst the ones listed above")
+    print("please input the proper terms")
